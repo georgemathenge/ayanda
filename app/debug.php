db9602ea9a45824d9fab7fe979d05cfeeff45ab0
George Mathenge <47426850+georgemathenge@users.noreply.github.com>
2025-12-18 12:18:46 +0300

    Add debug.php for displaying database info

---
+++ app/debug.php
@@ -0,0 +1,6 @@
+<?php
+require_once('wp-config.php');
+echo "<h1>MYSTERY SOLVED</h1>";
+echo "Your App is using Database: <h2 style='color:red'>" . DB_NAME . "</h2>";
+echo "Your App is using Host: <strong>" . DB_HOST . "</strong><br>";
+?>
